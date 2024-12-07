b45e216a281105db4042c29c35f1e4597f1c0b30
Ricky Klasson <mrklasson@gmail.com>
2024-12-07 09:56:57 +0100

    Make telescope search in all dirs except .git

--- env/.config/nvim/after/plugin/telescope.lua
+++ env/.config/nvim/after/plugin/telescope.lua
@@ -3,7 +3,11 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find p
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
 -- TODO: This grep search does not seem to work.
 vim.keymap.set('n', '<leader>ps', function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.live_grep({
+        additional_args = function()
+            return { "--hidden", "--glob", "!.git/*" }
+        end,
+    });
 end)
 
 require("telescope").setup()

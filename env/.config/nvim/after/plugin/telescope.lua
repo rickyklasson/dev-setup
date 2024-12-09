85c82428a17f816f67a10398b6a503d29fb9aadb
Ricky Klasson <ricky.klasson@cgglobal.com>
2024-12-09 10:58:26 +0100

    Minor changes

--- env/.config/nvim/after/plugin/telescope.lua
+++ env/.config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find project files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
--- TODO: This grep search does not seem to work.
 vim.keymap.set('n', '<leader>ps', function() 
     builtin.live_grep();
 end)
--- env/.config/nvim/lua/main/remap.lua
+++ env/.config/nvim/lua/main/remap.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>cs", ":nohlsearch<CR>", { silent = true })
+
+-- Yank to system clipboard.
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])

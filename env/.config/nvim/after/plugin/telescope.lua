a51778905be4a0f77a018db627ead341f9019b2a
Ricky Klasson <mrklasson@gmail.com>
2024-12-09 20:29:21 +0100

    Improved telescope keymappings

--- env/.config/nvim/after/plugin/telescope.lua
+++ env/.config/nvim/after/plugin/telescope.lua
@@ -1,10 +1,8 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find project files' })
-vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
-vim.keymap.set('n', '<leader>ps', function() 
-    builtin.live_grep();
-end, { desc = "Live grep with empty search field" })
-vim.keymap.set('n', '<leader>py', function()
+vim.keymap.set('n', '<leader>sp', builtin.find_files, { desc = 'Telescope find project files' })
+vim.keymap.set('n', '<leader>sg', builtin.git_files, { desc = 'Telescope find git files' })
+vim.keymap.set('n', '<leader>sl', builtin.live_grep, { desc = "Live grep with empty search field" })
+vim.keymap.set('n', '<leader>sy', function()
     local yanked_text = vim.fn.getreg('"0') -- Get the content of the yank register
     if yanked_text ~= "" then
         builtin.live_grep({ default_text = yanked_text })

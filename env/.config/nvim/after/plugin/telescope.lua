75a600baca99504cf522e5b946ecbbe6d22b74ab
Ricky Klasson <ricky.klasson@cgglobal.com>
2024-12-09 14:58:13 +0100

    Fuzzy history searching using fzf

--- env/.config/nvim/after/plugin/telescope.lua
+++ env/.config/nvim/after/plugin/telescope.lua
@@ -3,7 +3,16 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find p
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find git files' })
 vim.keymap.set('n', '<leader>ps', function() 
     builtin.live_grep();
-end)
+end, { desc = "Live grep with empty search field" })
+vim.keymap.set('n', '<leader>py', function()
+    local yanked_text = vim.fn.getreg('"0') -- Get the content of the yank register
+    if yanked_text ~= "" then
+        builtin.live_grep({ default_text = yanked_text })
+    else
+        builtin.live_grep()
+    end
+end, { desc = "Live grep for yanked text" })
+
 
 require("telescope").setup({
     defaults = {
--- runs/libs.sh
+++ runs/libs.sh
@@ -1,5 +1,5 @@
 sudo apt -y update
-sudo apt -y install git ripgrep fdclone tmux
+sudo apt -y install git ripgrep fdclone tmux fzf
 
 # Install exa.
 wget -c http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb

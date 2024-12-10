b7b2cfb9ed6409143cffcbd2182517d8f69ec89e
Ricky Klasson <ricky.klasson@cgglobal.com>
2024-12-10 09:12:26 +0100

    Enabled cursorline

--- env/.config/nvim/lua/main/set.lua
+++ env/.config/nvim/lua/main/set.lua
@@ -7,6 +7,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.cursorline = true
 
 vim.opt.smartindent = true
 
@@ -29,5 +30,4 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "120"
-
 

063a2522625f083762fb06c4af389dc778d1842d
Ricky Klasson <ricky.klasson@cgglobal.com>
2024-12-16 18:18:53 +0100

    Unf: Moving over to lazy plugin manager

--- env/.config/nvim/lua/main/remap.lua
+++ env/.config/nvim/lua/main/remap.lua
@@ -1,4 +1,3 @@
-vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>cs", ":nohlsearch<CR>", { silent = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -6,10 +5,21 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]]) -- Yank to system clipboard.
 vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 
 -- Avoid automatic comment insertion when pressing o/O in NORMAL mode, and enter in INSERT mode.
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "*",
     command = "setlocal formatoptions-=o formatoptions-=r"
+})
+
+-- Highlight when yanking (copying) text
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
--- env/.config/nvim/lua/main/set.lua
+++ env/.config/nvim/lua/main/set.lua
@@ -1,7 +1,12 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = false
+
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.showmode = false
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -10,6 +15,10 @@ vim.opt.expandtab = true
 vim.opt.cursorline = true
 
 vim.opt.smartindent = true
+vim.opt.breakindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.wrap = false
 
@@ -27,7 +36,9 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 800
 
 vim.opt.colorcolumn = "120"
+
 

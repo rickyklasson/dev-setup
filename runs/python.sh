456e88fd1801bc381091ecfe25c293ef6211b970
Ricky Klasson <ricky.klasson@cgglobal.com>
2024-12-18 16:05:45 +0100

    Funcional pylsp

--- runs/python.sh
+++ runs/python.sh
@@ -5,3 +5,5 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 PYTHON_VERSION="3.11"
 sudo apt install python3.11 python${PYTHON_VERSION}-venv python${PYTHON_VERSION}-distutils
 
+# Install venv for the current python installation.
+sudo apt install python3.8-venv

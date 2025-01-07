b76751262957bd8ba043d54406dce135ccb8744e
Ricky Klasson <ricky.klasson@cgglobal.com>
2025-01-07 09:46:12 +0100

    Python3.10 and tmux status

--- runs/python.sh
+++ runs/python.sh
@@ -2,6 +2,9 @@
 
 sudo add-apt-repository ppa:deadsnakes/ppa
 
+PYTHON_VERSION="3.10"
+sudo apt install python3.10 python${PYTHON_VERSION}-venv python${PYTHON_VERSION}-distutils
+
 PYTHON_VERSION="3.11"
 sudo apt install python3.11 python${PYTHON_VERSION}-venv python${PYTHON_VERSION}-distutils
 

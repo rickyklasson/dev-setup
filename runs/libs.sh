49ca468854d9b1f02828f3956fd039f1916b189b
Ricky Klasson <ricky.klasson@cgglobal.com>
2025-01-08 07:41:50 +0100

    Tmux resurrect

--- runs/libs.sh
+++ runs/libs.sh
@@ -6,3 +6,5 @@ wget -c http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0
 sudo apt-get install ./exa_0.9.0-4_amd64.deb
 rm ./exa_0.9.0-4_amd64.deb
 
+# Install tmux plugin manager.
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm

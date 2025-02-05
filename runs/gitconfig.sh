093b7cc4e3f26e151818c74b5b943cf4a04ae58e
Ricky Klasson <ricky.klasson@cgglobal.com>
2025-02-05 07:36:22 +0100

    Broke out git config into shell script

---
+++ runs/gitconfig.sh
@@ -0,0 +1,11 @@
+git config --global alias.b branch
+git config --global alias.s status
+git config --global alias.c checkout
+git config --global alias.d diff
+git config --global alias.dt difftool
+git config --global alias.co commit
+git config --global alias.ca "commit --amend"
+git config --global alias.can "commit --amend --no-edit"
+git config --global alias.l "log --oneline --graph --all --decorate --color"
+git config --global diff.tool nvim
+git config --global difftool.nvim.cmd "nvim -d \$LOCAL \$REMOTE"

df556ca04558d34488a7514bb8aeace7a769a089
Ricky Klasson <ricky.klasson@cgglobal.com>
2025-02-18 08:47:59 +0100

    Replace vim-fugitive by gitsigns

--- config/.config/nvim/lua/main/lazy.lua
+++ config/.config/nvim/lua/main/lazy.lua
@@ -80,7 +80,12 @@ require('lazy').setup({
 
     {'theprimeagen/harpoon'},
     {'mbbill/undotree'},
-    {'tpope/vim-fugitive'},
+    {
+        'lewis6991/gitsigns.nvim',
+        config = function ()
+            require('gitsigns').setup()
+        end
+    },
     {
         'echasnovski/mini.map',
         version = false,
